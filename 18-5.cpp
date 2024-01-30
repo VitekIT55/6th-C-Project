7fe8fa312f6877e51916c65ced99dfd8e55ac376
VitekIT55 <trakmanic80@gmail.com>
2024-01-30 16:22:10 +0300

    fix1

--- 18-5.cpp
+++ 18-5.cpp
@@ -1,12 +1,11 @@
 ﻿#include <iostream>
 
 //using namespace std;
-
 class Game
 {
 private:
-    char Name;
-    int Score = 0;
+    char Name{};
+    int Score{};
     int MasSize;
     int* sc = new int[MasSize];
     char* nm = new char[MasSize];
@@ -15,35 +14,39 @@ public:
     {
         std::cout << "Введите размер массива: "; std::cin >> MasSize;
     }
-    void SetScore()
+    void SetNameAndScore()
     {
         for (int i = 0; i < MasSize; i++)
         {
+            std::cout << "////\n";
+            std::cout << "Введите Имя " << i + 1 << " игрока: "; std::cin >> Name;
+            nm[i] = Name;
             std::cout << "Введите Счёт " << i + 1 << " игрока: "; std::cin >> Score;
-            char* String = new char[Name];
             sc[i] = Score;
         }
     }
-    void SetName()
+    void BubbleSort() 
     {
-        for (int i = 0; i < MasSize; i++)
-        {
-            std::cout << "Введите Имя " << i + 1 << " игрока: "; std::cin >> Name;
-            char* String = new char[Name];
-            nm[i] = Name;
-            //+static_cast<char>(Score);
-        //    char* String = new char[char(Name + " " + Score)];
-            //const char Str = char(Name + " " + Score);
-            //std::cout << String;
-            //std::cout << "\n";
-        //    p[i] = String;
+        int tempsc, tempnm;
+        for (int i = 0; i < MasSize - 1; i++) {
+            for (int j = 0; j < MasSize - i - 1; j++) {
+                if (sc[j] < sc[j + 1]) {
+                    // меняем элементы местами
+                    tempsc = sc[j];
+                    sc[j] = sc[j + 1];
+                    sc[j + 1] = tempsc;
+                    tempnm = nm[j];
+                    nm[j] = nm[j + 1];
+                    nm[j + 1] = tempnm;
+                }
+            }
         }
     }
     void ShowMas()
     {
         for (int i = 0; i < MasSize; i++)
         {
-            std::cout << i + 1 << " число массива: " << sc[i] << " - " << nm[i] << std::endl;
+            std::cout << i + 1 << " игрок: " << nm[i] << " со счётом " << sc[i] << std::endl;
         }
     }
 };
@@ -53,8 +56,9 @@ int main()
     system("chcp 1251>nul");
     Game G;
     G.MassiveSize();
-    G.SetScore();
-    G.SetName();
+    G.SetNameAndScore();
+    G.BubbleSort();
+    std::cout << "------\n";
     G.ShowMas();
 //    delete p;
 }
